f27e780e98154800e77b74e2403074346ef8bf00
Nicholandn22 <nicholas.dwinata@ti.ukdw.ac.id>
2024-05-12 15:57:59 +0700

    Pindah Data

---
+++ login.php
@@ -0,0 +1,33 @@
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="login.css" />
+</head>
+<body>
+<header>
+        <div class="navigation">
+          <div id="judul">
+            <img
+              src="drive-download-20240310T064708Z-001/Logo Mytic (White).png"
+              alt=""
+            />
+            <!-- <h1>My.Tic</h1> -->
+          </div>
+          <div id="kanan">
+            <ul>
+              <a href="main.html">Utama</a>
+              <a href="#">List Konser</a>
+              <a href="main.html">Tentang Kami</a>
+              <a href="#"><img src="drive-download-20240310T064708Z-001/Logo Mytic (White).png" alt="">Login</a>
+            </ul>
+          </div>
+        </div>
+        <!-- <h1 id="WELCOME">WELCOME</h1> -->
+    </header>
+    
+</body>
+</html>
