0e3340f83bc0c05eec9c1bdaa53a4e3465b063ef
Nicholandn22 <nicholas.dwinata@ti.ukdw.ac.id>
2024-05-12 16:58:21 +0700

    login page n css

--- login.php
+++ login.php
@@ -22,12 +22,37 @@
               <a href="main.html">Utama</a>
               <a href="#">List Konser</a>
               <a href="main.html">Tentang Kami</a>
-              <a href="#"><img src="drive-download-20240310T064708Z-001/Logo Mytic (White).png" alt="">Login</a>
+              <a href="#"><img src="logo/user.svg" alt=""></a>
+              <a href="#">Login</a>
             </ul>
           </div>
         </div>
-        <!-- <h1 id="WELCOME">WELCOME</h1> -->
     </header>
+
+  <main>
+    <div class="containerBox">
+      <form action="">
+      <h2>Log in</h2>
+      <h4>Email</h4>
+      <input type="text" placeholder="Masukkan Email Anda">
+      <h4>Your Password</h4>
+      <input type="password" placeholder="Masukkan Password Anda">
+      <button>
+  <span class="button_top">Button
+  </span>
+</button>
+<h5>By continuing, you agree to the <a href="#">Terms of use</a> and <a href="#">Privacy Policy</a></h5>
+    <div class="opsi2">
+      <h5>Do not have an account ? <a href="#">Create account</a></h5>
+      <h5><a href="#">Forget your password</a></h5>
+    </div>
+    </form>
+
+    </div>
+  </main>
+
+
+
     
 </body>
 </html>
